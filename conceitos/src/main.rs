08d45bd84aaa2c76b963c4816f4c61ceae204442
RafaelMotaMoraes <rafaelmota622@gmail.com>
2025-02-03 14:41:46 -0300

    Variáveis e mutabilidade

---
+++ conceitos/src/main.rs
@@ -0,0 +1,21 @@
+const UMA_HORA_EM_SEGUNDOS: i32 = 1 * 60 * 60;
+
+fn main() {
+    const UMA_HORA_EM_SEGUNDOS: i32 = 1 * 60 * 60; // Escopo interno, sobrescreve o escopo externo
+                                                   // Variáveis e mutabilidade
+    println!("Inicio do programa");
+    let x = 5;
+    println!("O valor de x é: {x}");
+
+    // x = 6; // x não é mutável, x é constante
+
+    let x = 666;
+    println!("O valor de x é: {x}");
+
+    let mut y = 5;
+    println!("O valor de y é: {y}");
+    y = 6;
+    println!("O valor de y é: {y}");
+
+    println!("Uma hora tem {UMA_HORA_EM_SEGUNDOS} segundos!");
+}
